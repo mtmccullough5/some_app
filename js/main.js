b38fb764439d8fdaad99a80f2e555b8c82e78038
Matthew McCullough <mcculloughmatthew5@gmail.com>
2017-08-24 20:59:11 -0600

    Add Cart Toggle

--- js/main.js
+++ js/main.js
@@ -1,54 +1,72 @@
 // Jquery
 // General form $(selector).action
 $(document).ready(function(){
-    $('#plan').on('change', function(){
-        var priceText = "$10.00 /mo";
-
-        switch(this.value){
-            case "monthly":
-                priceText = "$30.00 /mo";
-                break;
-            case "quarterly":
-                priceText = "$25.00 /mo";
-                break;
-            case "yearly":
-                priceText = "$20.00 /mo";
-                break;
-        }
-        $('#price').text(priceText)
-    });    
-    $('#add').on('click', function(){
-        var plan = $('#plan');
-        var installment = plan.val();
-        var price = $('#price').text();
-        var inCart = $('#incart');
-        var numeric = price.replace(/[[A-Za-z$\/\s]/g, '');
-        var data = 'data-price="' + numeric + '" data-plan="'+ installment +'"';
-        inCart.append('<li class="entry"' + data + '>' + installment + '-' + price + '</li>');
-        updateTotal();
-    });
-
-    function updateTotal() {
-        var total = 0;
-        $('.entry').each( function(index,entry){
-            var data = $(entry).data();
-            var price = parseFloat(data.price)
-            var installement = data.plan;
-            switch (installement) {
-                case "monthly":
-                    total += price;    
-                    break;
-                case "quarterly":
-                    total += price * 3;
-                    break;
-                case "yearly":
-                    total += price *12;
-                    break;
-            }
-            $('#carttotal').text('$' + total)
-            console.log(data)
-        })
+  $('#plan').on('change', function(){
+    var priceText = "$10.00 /mo";
+    switch(this.value){
+      case "monthly":
+        priceText = "$30.00 /mo";
+        break;
+      case "quarterly":
+        priceText = "$25.00 /mo";
+        break;
+      case "yearly":
+        priceText = "$20.00 /mo";
+        break;
     }
+    $('#price').text(priceText)
+  });    
+  $('#add').on('click', function(){
+    var plan = $('#plan');
+    var installment = plan.val();
+    var price = $('#price').text();
+    var inCart = $('#incart');
+    var numeric = price.replace(/[[A-Za-z$\/\s]/g, '');
+    var data = 'data-price="' + numeric + '" data-plan="'+ installment +'"';
+    inCart.append('<li class="entry"' + data + '>' 
+      + installment + '-' 
+      + price + 
+      '\t <button class="remove">x</button></li>');
+    updateTotal();
+  });
+  $(document).on('click', '.remove',function(){
+    $(this).parent('li').remove();
+    updateTotal();
+  });
+  $('#empty').on('click', function(){
+    $('#in-cart').empty();
+    updateTotal();
+  })
+  $('#display-cart').hover(function(){
+    $('#cart').slideToggle();
+  })
+  function updateTotal() {
+    var total = 0;
+    var entries = $('.entry')
+    if(entries.length)
+      $('#empty').show;
+    else
+      $('#empty').hide();
+
+    entries.each( function(index,entry){
+      var data = $(entry).data();
+      var price = parseFloat(data.price)
+      var installement = data.plan;
+      switch (installement) {
+        case "monthly":
+          total += price;    
+          break;
+        case "quarterly":
+          total += price * 3;
+          break;
+        case "yearly":
+          total += price *12;
+          break;
+      }
+      $('#carttotal').text('$' + total)
+      console.log(data)
+    })
+  }
 
 });
 
@@ -57,14 +75,14 @@ $(document).ready(function(){
 
 var btn = document.getElementById('btn');
 btn.addEventListener('click', function() {
-    var firstname = document.getElementById('first_name');
-    var lastname = document.getElementById('last_name');
-    var list = document.getElementById('people'); 
-    var li = document.createElement('li');
-    li.innerHTML = lastname.value + ', ' + firstname.value;
-    list.appendChild(li);
-    firstname.value = "";
-    lastname.value = "";
+  var firstname = document.getElementById('first_name');
+  var lastname = document.getElementById('last_name');
+  var list = document.getElementById('people'); 
+  var li = document.createElement('li');
+  li.innerHTML = lastname.value + ', ' + firstname.value;
+  list.appendChild(li);
+  firstname.value = "";
+  lastname.value = "";
 });
 
 
